386adec10f93ac7d2e6bd41169ba391aff30abff
MaxRK <max.rekar@gmail.com>
2025-11-28 13:07:41 +0100

    initialisation Sprint boot et Angular

---
+++ librairie-back/src/main/java/g1/librairie_back/LibrairieBackApplication.java
@@ -0,0 +1,13 @@
+package g1.librairie_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrairieBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrairieBackApplication.class, args);
+	}
+
+}
---
+++ librairie-back/src/test/java/g1/librairie_back/LibrairieBackApplicationTests.java
@@ -0,0 +1,13 @@
+package g1.librairie_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrairieBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
