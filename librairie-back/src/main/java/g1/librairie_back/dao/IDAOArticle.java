f1f96cf25790e13ef13fb7bcb86b4c9f5f5f5e72
Viviana <viviana.srr21@gmail.com>
2025-12-01 12:27:24 +0100

    Ajout des IDAO Article et Auteur

---
+++ librairie-back/src/main/java/g1/librairie_back/dao/IDAOArticle.java
@@ -0,0 +1,20 @@
+package g1.librairie_back.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import g1.librairie_back.model.Article;
+import g1.librairie_back.model.Livre;
+import g1.librairie_back.model.Papeterie;
+
+public interface IDAOArticle extends JpaRepository<Article,Integer>{
+	
+    @Query("from Livre")
+	public List<Livre> findAllLivre();
+	
+	@Query("from Papeterie")
+	public List<Papeterie> findAllPapeterie();
+
+}
