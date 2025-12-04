e9be4845be75cb1090008ae6bb8913f03407e777
MaxRK <max.rekar@gmail.com>
2025-12-04 15:29:04 +0100

    ajout sécurité front

--- librairie-front/src/app/guard/auth-client-guard.ts
+++ librairie-front/src/app/guard/auth-client-guard.ts
@@ -1,19 +1,29 @@
-import { inject } from "@angular/core";
+import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
-import { AuthService } from "../service/auth-service";
+import { AuthService } from '../service/auth-service';
 
 export const AuthClientGuard: CanActivateFn = (route, state) => {
-    const router = inject(Router);
-    const authService = inject(AuthService);
+  const router = inject(Router);
+  const authService = inject(AuthService);
 
-    if(authService.token){
-      if(authService.role=="[ROLE_CLIENT]"){
-        return true;
+  if(authService.token){
+    if(authService.role=="[ROLE_CLIENT]"){
+      return true;
+    } else {
+      if(!authService.role){
+        alert("Vous n'êtes pas connecté à un compte client")
+        return router.createUrlTree([ '/connexion' ]);
+
       } else {
         alert("Votre compte n'est pas client, accès refusé");
+
+        return router.createUrlTree([ '/home' ]);
+        
       }
     }
-
-  // Navigation à utiliser dans les guard
+  }
   return router.createUrlTree([ '/home' ]);
 }
+
+  // Navigation à utiliser dans les guard
+  
